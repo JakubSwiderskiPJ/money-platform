fb488ee48865c38784acd168a1049a30d05793c1
JakubSwiderskiPJ <s19443@pjwstk.edu.pl>
2022-05-12 13:51:25 +0200

    add users mock

---
+++ src/mocks/users.ts
@@ -0,0 +1,12 @@
+import { v4 } from "uuid";
+
+export const Users = [
+    { ID: v4() },
+    { ID: v4() },
+    { ID: v4() },
+    { ID: v4() },
+    { ID: v4() },
+    { ID: v4() },
+    { ID: v4() },
+    { ID: v4() },
+];
